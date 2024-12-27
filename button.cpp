e1eb62cdf48ddfddd79bb8e08c91c2845d77312f
sumanhosad <sumanhosad98@gmail.com>
2024-12-27 16:53:09 +0530

    cleaning

--- button.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtkmm/application.h"
-#include "includes/buttons.h"
-
-int main(int argc, char *argv[]) {
-  auto app = Gtk::Application::create("yes");
-
-  return app->make_window_and_run<Buttons>(argc, argv);
-}
