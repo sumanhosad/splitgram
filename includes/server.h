e8546d1ba45dc0a509ee1efee167ba8a8253a672
sumanhosad <sumanhosad98@gmail.com>
2024-12-31 09:55:40 +0530

    server coding started

---
+++ includes/server.h
@@ -0,0 +1,5 @@
+#include <cstdlib>      //for sockaddr_in
+#include <iostream>     //for sockaddr_in
+#include <netinet/in.h> //for sockaddr_in
+#include <sys/socket.h> //for socket
+#include <unistd.h>     //for read
